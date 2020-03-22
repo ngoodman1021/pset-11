4173e4ade121c33abf52ad80696fd98323892f51
ngoodman1021 <32653505+ngoodman1021@users.noreply.github.com>
2020-03-21 21:20:47 -0400

    fifth commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,52 +1,55 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Exercises {
 
   public int findMe(int[] list, int target) {
     if (Arrays.asList(list).contains(null) || Arrays.asList(list) == null) {
-			return -1;
+	    return -1;
+	}
+     for (int i = 0; i < list.length; i++) {
+	if (list[i] == target) {
+		return i;
 		}
-
-		for (int i = 0; i < list.length; i++) {
-			if (list[i] == target) {
-				return i;
-			}
-		}
+	}
+	  
     return -1;
   }
 
   public int findMe(ArrayList<String> list, String target) {
     if (list.contains(null) || list == null) {
-			return -1;
-		}
+	return -1;
+	}
 
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).equals(target)) {
-				return i;
-			}
-		}
+       for (int i = 0; i < list.size(); i++) {
+	 if (list.get(i).equals(target)) {
+		return i;
+	}
+   }
     return -1;
   }
 
   public int findMeFaster(ArrayList<Integer> list, int target) {
     if (list.contains(null) || list == null) {
-			return -1;
-		}
-
-		int low = 0;
-		int high = list.size() - 1;
-		int index = -1;
-	while (low <= high) {
-		int mid = (low + high) / 2;
-		if (list.get(mid) < target) {
-			low = mid + 1;
-		} else if (list.get(mid) > target) {
-			high = mid - 1;
-		} else if (list.get(mid) == target) {
-			index = mid;
-			break;
-		}
+	return -1;
 	}
+
+	int low = 0;
+	int high = list.size() - 1;
+	int index = -1;
+    
+    while (low <= high) {
+	int mid = (low + high) / 2;
+	if (list.get(mid) < target) {
+		low = mid + 1;
+	} else if (list.get(mid) > target) {
+		high = mid - 1;
+	} else if (list.get(mid) == target) {
+		index = mid;
+		break;
+	}
+		}
      return index;
   }
 
