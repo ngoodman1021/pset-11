8fabd7edd106e9be8332ea58d72d0edfa919bb7f
ngoodman1021 <32653505+ngoodman1021@users.noreply.github.com>
2020-03-24 11:42:05 -0400

    thirteenth commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -205,7 +205,33 @@ public class Exercises {
   }
 
   public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
-    return null;
+    if (Arrays.asList(list).contains(null) || Arrays.asList(list) == null) {
+	return null;
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+	String smallest = list.get(i);
+	int smallestIndex = i;
+	   for (int j = i; j < list.size(); j++) {
+		String value = list.get(j);
+		      if (value.compareTo(smallest) < 0) {
+			smallest = value;
+			smallestIndex = j;
+		}
+	}
+
+	if (smallestIndex != i) {
+	    String head = list.get(i);
+	    list.set(i, smallest);
+	    list.set(smallestIndex, head);
+	}
+     }
+
+     if (!ascending) {
+	Collections.reverse(list);
+     }
+
+	return list;
   }
 
   public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
