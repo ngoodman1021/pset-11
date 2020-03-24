2d6cace8dc6695eb432580e3da9a72962348499f
ngoodman1021 <32653505+ngoodman1021@users.noreply.github.com>
2020-03-24 11:57:45 -0400

    fifteenth commit

--- src/Exercises.java
+++ src/Exercises.java
@@ -267,9 +267,89 @@ public class Exercises {
     return list;
 	}
   }
+
 //for exercise 11
-  
+  private void halves(ArrayList<Integer> left, ArrayList<Integer> right, ArrayList<Integer> complete) {
+    int lIndex = 0;
+    int rIndex = 0;
+    int completeAIndex = 0;
+
+    while (lIndex < left.size() && rIndex < right.size()) {
+	if ((left.get(leftIndex).compareTo(right.get(rightIndex))) < 0) {
+	   complete.set(completeAIndex, left.get(lIndex));
+	   lIndex++;
+	  } else {
+	    complete.set(completeAIndex, right.get(rIndex));
+	    rIndex++;
+	}
+			completeAIndex++;
+   }
+
+   ArrayList<Integer> rest;
+        int restIndex;
+	if (lIndex >= left.size()) {
+	    rest = right;
+	    restIndex = rIndex;
+         } else {
+	    rest = left;
+	    restIndex = lIndex;
+    }
+
+    for (int i = restIndex; i < rest.size(); i++) {
+	 complete.set(completeAIndex, rest.get(i));
+	 completeAIndex++;
+       }
+    }
+
 public String[] merge(String[] list, boolean ascending) {
-    return null;
+    if (Arrays.asList(list).contains(null) || Arrays.asList(list) == null) {
+	String[] temp = new String[0];
+	return temp;
+    }
+
+    if (list.length >= 2) {
+	String[] left = new String[list.length / 2];
+	String[] right = new String[list.length - list.length / 2];
+
+	for (int i = 0; i < left.length; i++) {
+	     left[i] = list[i];
+	}
+
+  	for (int i = 0; i < right.length; i++) {
+	     right[i] = list[i + list.length / 2];
+	}
+
+	merge(left, true);
+	merge(right, true);
+	combine(list, left, right);
+    }
+
+    if (!ascending) {
+	int i;
+	String t;
+	int x = list.length;
+	for (i = 0; i < x / 2; i++) {
+	    t = list[i];
+	    list[i] = list[x - i - 1];
+	    list[x - i - 1] = t;
+	}
+    }
+	return list;
+  }
+
+// for exerise 12 
+private static void combine(String[] list, String[] left, String[] right) {
+    int j = 0;
+    int k = 0;
+    
+    for (int i = 0; i < list.length; i++) {
+	if (k >= right.length || (j < left.length && left[j].compareToIgnoreCase(right[k]) < 0)) {
+	   list[i] = left[j];
+	   j++;
+	} else {
+	   list[i] = right[k];
+	   k++;
+	}
+     }
   }
 }
